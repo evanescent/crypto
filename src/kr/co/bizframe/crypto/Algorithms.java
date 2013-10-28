f1c5bb045819c07dba4a52bf0c8154ced2fcd983
hakju <hakju@quicket.co.kr>
2013-10-28 22:55:58 +0900

    주석 (3)

--- src/kr/co/bizframe/crypto/Algorithms.java
+++ src/kr/co/bizframe/crypto/Algorithms.java
@@ -13,35 +13,78 @@ import kr.co.bizframe.crypto.ciphers.CipherManager;
 import kr.co.bizframe.crypto.digests.DigestManager;
 
 /**
- * 
+ * 지원하는 알고리즘을 담고 있는 클래스
  */
 public class Algorithms {
 
 	public static enum Type {
-		BLOCK_CIPHER, MODE, MAC, DIGEST
+		/**
+		 * 블록 암호 알고리즘
+		 */
+		BLOCK_CIPHER, 
+		
+		/**
+		 * 운용 모드
+		 */
+		MODE, 
+		
+		/**
+		 * 메시지 인증 코드
+		 */
+		MAC,
+		
+		/**
+		 * 해쉬 함수
+		 */
+		DIGEST
 	}
 
 	private Map<String, Algorithm> algorithms = new HashMap<String, Algorithm>();
 
+	/**
+	 * 블록 암호 알고리즘을 추가한다.
+	 * 
+	 * @param name 블록 암호 알고리즘명
+	 * @param clazz 블록 암호 알고리즘 구현 클래스
+	 */
 	public void addBlockCipher(String name, Class<? extends BlockCipher> clazz) {
 		Algorithm algo = new BlockCipherAlgorithm(name, Type.BLOCK_CIPHER, clazz, null);
 		algorithms.put(name, algo);
 	}
 
-	public void addBlockCipher(String name, Class<? extends BlockCipher> clazz, Class<? extends BlockCipher> mode) {
-
+	/**
+	 * 블록 암호 알고리즘을 추가한다.
+	 * 
+	 * @param name 블록 암호 알고리즘명
+	 * @param clazz 블록 암호 알고리즘 구현 클래스
+	 * @param mode 운용 모드
+	 */
+	public void addBlockCipher(String name, Class<? extends BlockCipher> clazz, 
+			Class<? extends BlockCipher> mode) {
 		BlockCipherAlgorithm algo = new BlockCipherAlgorithm(name, Type.BLOCK_CIPHER, clazz, mode);
 		algorithms.put(name, algo);
 	}
 
+	/**
+	 * 해쉬 함수를 추가한다.
+	 * 
+	 * @param name 해쉬 함수명
+	 * @param clazz 해쉬 함수 구현 클래스
+	 */
 	public void addDigest(String name, Class<? extends Digest> clazz) {
-
 		DigestAlgorithm algo = new DigestAlgorithm(name, Type.DIGEST, clazz);
 		algorithms.put(name, algo);
 	}
 
+	/**
+	 * 주어진 알고리즘명에 해당하는 블록 암호 알고리즘 구현을 반환한다.
+	 * 
+	 * @param name 블록 암호 알고리즘명
+	 * @return 해당하는 알고리즘에 대한 구현
+	 * @throws NullPointerException 해당 알고리즘명이 등록되어 있지 않은 경우
+	 * @throws IllegalStateException 해당 알고리즘에 대한 구현체를 생성할 수 없는 경우
+	 */
 	public CipherManager getBlockCipher(String name) {
-
 		BlockCipherAlgorithm algo = (BlockCipherAlgorithm) algorithms.get(name);
 		if (algo == null) {
 			throw new NullPointerException("algorithm is null");
@@ -53,19 +96,25 @@ public class Algorithms {
 		BlockCipher cipher = null;
 		try {
 			cipher = blockClass.newInstance();
-
 			if (modeClass != null)
 				cipher = modeClass.getConstructor(blockClass).newInstance(cipher);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new IllegalStateException(e);
 		}
 		CipherManager manager = new CipherManager(cipher);
 		return manager;
 	}
 
+	/**
+	 * 주어진 알고리즘명에 해당하는 해쉬 함수 구현을 반환한다.
+	 * 
+	 * @param name 해쉬 함수 알고리즘명
+	 * @return 해당하는 알고리즘에 대한 구현
+	 * @throws NullPointerException 해당 알고리즘명이 등록되어 있지 않은 경우
+	 * @throws IllegalStateException 해당 알고리즘에 대한 구현체를 생성할 수 없는 경우
+	 */
 	public DigestManager getDigest(String name) {
-
 		DigestAlgorithm algo = (DigestAlgorithm) algorithms.get(name);
 		if (algo == null) {
 			throw new NullPointerException("algorithm=[" + name + "] is not supported.");
@@ -76,7 +125,7 @@ public class Algorithms {
 		try {
 			digest = digestClass.newInstance();
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new IllegalStateException(e);
 		}
 
 		DigestManager manager = new DigestManager(digest);
@@ -109,8 +158,8 @@ public class Algorithms {
 
 		private Class<? extends BlockCipher> modeClass;
 
-		public BlockCipherAlgorithm(String name, Type type, Class<? extends BlockCipher> clazz, Class<? extends BlockCipher> mode) {
-
+		public BlockCipherAlgorithm(String name, Type type, 
+				Class<? extends BlockCipher> clazz, Class<? extends BlockCipher> mode) {
 			super(name, type);
 			this.blockClass = clazz;
 			this.modeClass = mode;
@@ -131,7 +180,6 @@ public class Algorithms {
 		private Class<? extends Digest> digestClass;
 
 		public DigestAlgorithm(String name, Type type, Class<? extends Digest> clazz) {
-
 			super(name, type);
 			this.digestClass = clazz;
 		}
--- src/kr/co/bizframe/crypto/AsymmetricCipherKeyPair.java
+++ src/kr/co/bizframe/crypto/AsymmetricCipherKeyPair.java
@@ -7,25 +7,39 @@
 package kr.co.bizframe.crypto;
 
 /**
- * a holding class for public/private parameter pairs.
+ * 공개/비공개 쌍의 매개변수 클래스
  */
 public class AsymmetricCipherKeyPair {
 
 	private CipherParameters publicParam;
 	private CipherParameters privateParam;
 
-
+	/**
+	 * 주어진 매개변수를 사용하는 생성자
+	 * 
+	 * @param publicParam 공개키 매개변수
+	 * @param privateParam 비공개키 매개변수
+	 */
 	public AsymmetricCipherKeyPair(CipherParameters publicParam,
 			CipherParameters privateParam) {
 		this.publicParam = publicParam;
 		this.privateParam = privateParam;
 	}
 
+	/**
+	 * 공개키 매개변수를 반환한다.
+	 * 
+	 * @return 공개키 매개변수
+	 */
 	public CipherParameters getPublic() {
 		return publicParam;
 	}
 
-
+	/**
+	 * 비공개키 매개변수를 반환한다.
+	 * 
+	 * @return 비공개키 매개변수
+	 */
 	public CipherParameters getPrivate() {
 		return privateParam;
 	}
