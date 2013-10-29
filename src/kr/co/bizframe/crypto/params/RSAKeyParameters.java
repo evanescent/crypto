77e41b079496c0d5ed557f2140078da72568cb19
https://github.com/evanescent/crypto.git <cshy7@hanmail.net>
2013-10-30 00:04:06 +0900

    no message

--- src/kr/co/bizframe/crypto/params/RSAKeyParameters.java
+++ src/kr/co/bizframe/crypto/params/RSAKeyParameters.java
@@ -1,12 +1,28 @@
+/**
+ * Copyright (c) 2013-2014 Torpedo Corporations. All rights reserved.
+ *
+ * BizFrame and BizFrame-related trademarks and logos are
+ * trademarks or registered trademarks of Torpedo Corporations
+ */
 package kr.co.bizframe.crypto.params;
 
 import java.math.BigInteger;
 
+/**
+ * 
+ */
 public class RSAKeyParameters extends AsymmetricKeyParameter {
 
 	private BigInteger modulus;
 	private BigInteger exponent;
 
+	/**
+	 * 기본 생성자
+	 * 
+	 * @param isPrivate 개인키 설정 여부
+	 * @param modulus (n = p * q ) 공개키와 개인키에 포함되는 n 값
+	 * @param exponent ( e mod phi(n) == d , d:개인키 ) 공개키에 포함되는 e 값
+	 */
 	public RSAKeyParameters(boolean isPrivate, BigInteger modulus,
 			BigInteger exponent) {
 
@@ -16,10 +32,18 @@ public class RSAKeyParameters extends AsymmetricKeyParameter {
 		this.exponent = exponent;
 	}
 
+	/**
+	 * 공개키와 개인키에 포함되는 n 값을 반환한다.
+	 * @return 공개키와 개인키에 포함되는 n 값
+	 */
 	public BigInteger getModulus() {
 		return modulus;
 	}
 
+	/**
+	 * 개인키를 구하기 위한 e 값을 반환한다.
+	 * @return 공개키에 포함되는 e 값
+	 */
 	public BigInteger getExponent() {
 		return exponent;
 	}
